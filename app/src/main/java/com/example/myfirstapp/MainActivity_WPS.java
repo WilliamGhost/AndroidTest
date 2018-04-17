48d2c92f52bd8999900d52114c180c42683cd211
Pacheco Sanchez <w.pacheco.sanchez@accenture.com>
2018-04-17 13:53:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity_WPS.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity_WPS extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__wps);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        String message = editText1.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
